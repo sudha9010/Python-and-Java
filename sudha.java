47aa3c7216a82e7eb56f84525aeac71465c8445a
sudha9010 <132307139+sudha9010@users.noreply.github.com>
2024-03-21 20:54:03 +0000

    sudha

--- sudha.java
+++ sudha.java
@@ -1,6 +1,6 @@
 public class sudha {
     public static void main(String[] args) {
-        String input = "Status";
+        String input = "Sudha";
         for (int i = input.length(); i > 0; i--) {
             System.out.println(input.substring(0, i));
         }
