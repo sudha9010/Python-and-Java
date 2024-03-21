5e76b47a7accaf41110612ed00513093cc478a32
sudha9010 <132307139+sudha9010@users.noreply.github.com>
2024-03-21 20:50:12 +0000

    sudha

---
+++ sudha.java
@@ -0,0 +1,8 @@
+public class sudha {
+    public static void main(String[] args) {
+        String input = "Status";
+        for (int i = input.length(); i > 0; i--) {
+            System.out.println(input.substring(0, i));
+        }
+    }
+}
